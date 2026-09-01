c825c28b078853a95deb1dd1a3623d66ace73490
joy chowdhury <mjoy24236@gmail.com>
2026-09-01 22:48:32 +0600

    cron jobs setup done

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import userRoutes from "./routes/user_route.js";
 import NotificationRoutes from "./routes/notification_route.js";
 import postRoutes from "./routes/post_route.js";
 import path from "path"
+import job from "./lib/utils/cron.js"
 dotenv.config();
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -26,6 +27,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/notifications", NotificationRoutes);
+app.get("/health",(req,res)=>{
+  try {
+    res.status(200).send("ok")
+  } catch (error) {
+    console.log("error in checking health",error);
+    res.status(500).json("internal server error")
+  }
+})
 if(process.env.NODE_ENV==="production"){
   app.use(express.static(path.join(__dirname,"frontend","dist")));
   app.get("/{*any}",(req,res)=>{
@@ -35,4 +44,7 @@ if(process.env.NODE_ENV==="production"){
 app.listen(PORT, () => {
   console.log(`app is running on port ${PORT}`);
   connectDB();
+  if(process.env.NODE_ENV==="production"){
+    job.start()
+  }
 });
